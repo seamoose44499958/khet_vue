8c5f3349add026ec17d14bc5fbb8eb31285b488e
Prog <seamoose44499958@gmail.com>
2022-06-20 19:54:27 -0400

    Added piece class to represent data for pieces

--- src/composables/CommonUtil.js
+++ src/composables/CommonUtil.js
@@ -1,13 +1,30 @@
-export class Coord{
+export class Piece{
 
-    constructor(x,y) {
+    constructor(ref,angle,x,y,maxX,maxY) {
+        this.ref = ref;
+        this.maxX = maxX;
+        this.maxY = maxY;
         this.x = x;
         this.y = y;
+        if([0,90,180,270].includes(angle)) {
+            this._angle = angle;
+        }
+        else{
+            throw new Error("Improper angle");
+        }
+
+    }
+
+    get angle(){
+        return this._angle + "deg"
     }
 
     set x(value){
         if(typeof value !== 'number'){
             throw new TypeError("Coordinate x must be a number");
+        }
+        if(value > this.maxX || value < 0){
+            throw new Error("Coordinate is out of bounds of min and max");
         }
 
         this._x = value;
@@ -21,6 +38,9 @@ export class Coord{
         if(typeof value !== 'number'){
             throw new TypeError("Coordinate y must be a number");
         }
+        if(value > this.maxY || value < 0){
+            throw new Error("Coordinate is out of bounds of min and max");
+        }
 
         this._y = value;
     }
@@ -29,8 +49,49 @@ export class Coord{
         return this._y;
     }
 
-    toString(){
+    set maxX(value){
+        if(typeof value !== 'number'){
+            throw new TypeError("Coordinate y must be a number");
+        }
+
+        this._maxX = value;
+    }
+
+    get maxX(){
+        return this._maxX;
+    }
+
+    set maxY(value){
+        if(typeof value !== 'number'){
+            throw new TypeError("Coordinate y must be a number");
+        }
+
+        this._maxY = value;
+    }
+
+    get maxY(){
+        return this._maxY;
+    }
+
+    rotate180(){
+        this._angle = (this._angle +  180) % 360
+        this._x = this._maxX - this._x;
+        this._y = this._maxY - this._y;
+    }
+
+    getCoord(){
         return "#c" + this._x + "_" + this._y;
     }
 
+}
+
+export function rotateBoard180(...pieces){
+    for(let i = 0;i < pieces.length;i++){
+        if(pieces[i] instanceof Piece) {
+            pieces[i].rotate180();
+        }
+        else{
+            throw new TypeError("pieces array should only contain piece objects")
+        }
+    }
 }
