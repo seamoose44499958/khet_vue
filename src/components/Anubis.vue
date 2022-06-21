1a122107a86a318cefc2c376d71d8cb8d4a3d35e
Prog <seamoose44499958@gmail.com>
2022-06-20 23:34:40 -0400

    New piece

---
+++ src/components/Anubis.vue
@@ -0,0 +1,51 @@
+<template>
+  <p>𓁢</p>
+</template>
+
+<script>
+export default {
+  name: "anubis-piece",
+  data(){
+    return{
+      transform:""
+    }
+  },
+
+  props: {
+    color:{
+      type:String,
+      required:true
+    },
+
+    rotationAngle:{
+      type:String,
+      default:"0deg"
+    }
+  },
+  watch: {
+    rotationAngle: {
+      immediate: true,
+      handler() {
+        this.transform = "translate(0%,-50%) "
+        if(["0deg","90deg","270deg"].includes(this.$props.rotationAngle)){
+          this.transform += `rotate(${this.$props.rotationAngle})`;
+        }
+        else if(this.$props.rotationAngle === "180deg"){
+          this.transform += "scale(-1,1)";
+        }
+        else{
+          throw new Error("Improper angle set to Anubis, either 0deg,90deg,180deg,270deg");
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  p {
+    color:v-bind(color);
+    font-size: 4vh;
+    transform: v-bind(transform);
+  }
+</style>
